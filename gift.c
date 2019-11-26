83ea1237d284282a60b49186db7173db771253ef
israelqwe <israelcet@hotmail.com>
2019-11-26 12:17:54 -0600

    permBitsnames

--- gift.c
+++ gift.c
@@ -1,10 +1,13 @@
+/*codigo por Israel Martinez*/
 #include <stdio.h>
 #include <string.h>
-unsigned char plainText[]={'h','o','l','a','\0'};
+unsigned char GSbox[]={0x1,0xa,0x4,0xc,0x6,0xf,0x3,0x9,0x2,0xd,0xb,0x7,0x5,0x0,0x8,0xe};
+unsigned char plainText[]={'H','o','l','a','c','o','m','o'};
 unsigned char cadena[]={
 	0x9a, 0xc1, 0x99, 0x54, 0xce, 0x13, 0x19, 0xb3,
     0x54, 0xd3, 0x22, 0x04, 0x60, 0xf7, 0x1c, 0x1e,'\0'
 };
+
 int regresaNumero(int numero){
 	int resultado=0;
 	int primeraParte=4*(numero/16);
@@ -38,6 +41,42 @@ void pruebaregresaNumero(){
 		printf("%d--->%d\n",i,regresaNumero(i) );
 	}
 }
+unsigned char * GS(unsigned char * texto){
+	unsigned char salida[8];
+	int i=0;
+	unsigned char high;
+	unsigned char low;
+	if (strlen(texto)==8)
+	{
+		for (i = 0; i < 8; i++)
+			{	
+				//printf("%x\n",texto[i] );
+				high=(texto[i]&0xf0)>>4;
+				//printf("%x\n",high );
+				low=(texto[i]&0x0f);
+				high=GSbox[high];
+				low=GSbox[low];
+				printf("%x %x\n",high,low );
+				texto[i]=(high<<4)^low;
+				printf("salida: %x\n",texto[i] );
+				high=0x00;
+				low=0x00;
+			}	
+	}
+	//texto=salida;
+
+}
+unsigned char * PermBits(unsigned char * state){
+	int indice=0;
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; ++j)
+		{
+			
+
+		}
+	}
+}
 /*
 
 
@@ -45,9 +84,12 @@ void pruebaregresaNumero(){
 int main(int argc, char const *argv[])
 {
 	int i=0;
+	//imprimeCadena(plainText);
+	//imprimeCadenaHex(cadena);
+	printf("%x\n",(0x4<<4)^0x1);
+	GS(plainText);
 	imprimeCadena(plainText);
-	imprimeCadenaHex(cadena);
-	printf("%x\n",cadena[0]&0xf );
-	pruebaregresaNumero();
+	//pruebaregresaNumero();
+	//printf("%d\n",sizeof(plainText) );
 	return 0;
 }
